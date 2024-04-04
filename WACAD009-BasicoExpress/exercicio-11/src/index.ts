bc50971010b652038e01434aa8bef8b1302ba021
ademarcastro <2022002444@ifam.edu.br>
2024-04-04 15:52:26 -0400

    [REFACTOR] - Refaz a codificação do exercicio-11 do WACAD009, o mesmo apresentava a compilação do scss para css errado e também foi necessário reorganizar a estrutura do diretório

--- WACAD009-BasicoExpress/exercicio-11/src/index.ts
+++ WACAD009-BasicoExpress/exercicio-11/src/index.ts
@@ -1,43 +1,42 @@
-import express from 'express';
-import path from 'path';
-import router from './router/mainRouter';
-import { engine } from 'express-handlebars';
-import sassMiddleware from 'node-sass-middleware';
+import express from "express";
+import path from "path";
+import router from "./router/mainRouter";
+import { engine } from "express-handlebars";
+import sass from "node-sass-middleware";
 
 const app = express();
 const PORT = process.env.PORT || 4444;
 
 // Vetor de tecnologias
 export const technologies = [
-    { name: 'Express', type: 'Framework', poweredByNodejs: true },
-    { name: 'Laravel', type: 'Framework', poweredByNodejs: false },
-    { name: 'React', type: 'Library', poweredByNodejs: true },
-    { name: 'Handlebars', type: 'Engine View', poweredByNodejs: true },
-    { name: 'Django', type: 'Framework', poweredByNodejs: false },
-    { name: 'Docker', type: 'Virtualization', poweredByNodejs: false },
-    { name: 'Sequelize', type: 'ORM tool', poweredByNodejs: true },
+  { name: "Express", type: "Framework", poweredByNodejs: true },
+  { name: "Laravel", type: "Framework", poweredByNodejs: false },
+  { name: "React", type: "Library", poweredByNodejs: true },
+  { name: "Handlebars", type: "Engine View", poweredByNodejs: true },
+  { name: "Django", type: "Framework", poweredByNodejs: false },
+  { name: "Docker", type: "Virtualization", poweredByNodejs: false },
+  { name: "Sequelize", type: "ORM tool", poweredByNodejs: true },
 ];
 
 // Configuração de Handlebars
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, '../src/views/main/'));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "../src/views/main/"));
 
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'public', 'scss'),
-    dest: path.join(__dirname, 'public', 'css'),
-    outputStyle: 'compressed',
-    prefix: '/css',
-}));
+app.use(
+  sass({
+    src: `${__dirname}/../public/scss`,
+    dest: `${__dirname}/../public/css`,
+    outputStyle: "compressed",
+    prefix: "/css",
+  })
+);
 
-app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
-
-// Configuração para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(`${__dirname}/../public/css`));
 
 // Definição das Rotas
 app.use(router);
 
 app.listen(PORT, () => {
-    console.log(`Servidor está rodando na porta: ${PORT}.`);
+  console.log(`Servidor está rodando na porta: ${PORT}.`);
 });
