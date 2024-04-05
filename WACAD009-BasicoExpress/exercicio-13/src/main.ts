17b01045770063674c9dd26863605d86654655b1
ademarcastro <2022002444@ifam.edu.br>
2024-04-05 16:26:42 -0400

    [REFACTOR] - Corrige a lógica do create do exercicio-13 do WACAD009

--- WACAD009-BasicoExpress/exercicio-13/src/main.ts
+++ WACAD009-BasicoExpress/exercicio-13/src/main.ts
@@ -1,14 +1,15 @@
-import express from 'express';
+import express from "express";
 import produtoRouter from "./routers/produtoRouter";
 
 const app = express();
 const PORT = process.env.PORT || 4444;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Definição das rotas
 app.use(produtoRouter);
 
 app.listen(PORT, () => {
-    console.log(`Servidor está rodando na porta: ${PORT}.`);
-})
+  console.log(`Servidor está rodando na porta: ${PORT}.`);
+});
--- WACAD009-BasicoExpress/exercicio-13/src/routers/produtoRouter.ts
+++ WACAD009-BasicoExpress/exercicio-13/src/routers/produtoRouter.ts
@@ -1,15 +1,15 @@
-import { Router } from 'express';
-import produtoController from '../controllers/produtoController';
+import { Router } from "express";
+import produtoController from "../controllers/produtoController";
 
 const produtoRouter = Router();
 
 // Controlador Produto
-produtoRouter.get('/produto', produtoController.index);
-produtoRouter.get('/produto/create', produtoController.create);
-produtoRouter.post('/produto/create', produtoController.create);
-produtoRouter.get('/produto/update/:id', produtoController.update);
-produtoRouter.post('/produto/update/:id', produtoController.update);
-produtoRouter.get('/produto/:id', produtoController.read);
-produtoRouter.post('/produto/:id', produtoController.remove);
+produtoRouter.get("/produto", produtoController.index);
+produtoRouter.get("/produto/create", produtoController.create);
+produtoRouter.post("/produto/create", produtoController.create);
+produtoRouter.get("/produto/update/:id", produtoController.update);
+produtoRouter.post("/produto/update/:id", produtoController.update);
+produtoRouter.get("/produto/:id", produtoController.read);
+produtoRouter.delete("/produto/:id", produtoController.remove);
 
 export default produtoRouter;
