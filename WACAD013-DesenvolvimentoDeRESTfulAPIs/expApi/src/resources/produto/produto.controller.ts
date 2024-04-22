6e6c767b737d6aac87623b65284fe1e532d35643
AdemarCastro <ademar.castro.fh@gmail.com>
2024-04-22 19:12:10 -0400

    [FEAT] - Adiciona o exercicio-03 de WACAD013

--- WACAD013-DesenvolvimentoDeRESTfulAPIs/expApi/src/resources/produto/produto.controller.ts
+++ WACAD013-DesenvolvimentoDeRESTfulAPIs/expApi/src/resources/produto/produto.controller.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { checkNomeIsAvaliable, createProduto, listProdutos, readProduto } from "./produto.service";
-import { CreateProdutoDto } from "./produto.types";
+import { checkNomeIsAvaliable, createProduto, deleteProduto, listProdutos, readProduto, updateProduto } from "./produto.service";
+import { CreateProdutoDto, UpdateProdutoDto } from "./produto.types";
 
 const index = async (req: Request, res: Response) => {
+    const skip = req.query.skip ? parseInt(req.query.skip.toString()) : undefined;
+    const take = req.query.take ? parseInt(req.query.take.toString()) : undefined;
+
     try {
-        const produto = await listProdutos();
+        const produto = await listProdutos(skip, take);
         res.status(StatusCodes.OK).json(produto);
     } catch(err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
-}
+};
+
 const create = async (req: Request, res: Response) => {
     const produto = req.body as CreateProdutoDto; // as CreateProdutoDto
     try {
@@ -24,7 +28,8 @@ const create = async (req: Request, res: Response) => {
         console.log(err);
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
-}
+};
+
 const read = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
@@ -34,9 +39,34 @@ const read = async (req: Request, res: Response) => {
     } catch(err) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
-}
-const update = async (req: Request, res: Response) => {}
-const remove = async (req: Request, res: Response) => {}
+};
+
+const update = async (req: Request, res: Response) => {
+    const { id } = req.params ;
+    // const produto: UpdateProdutoDto = req.body;
+    const produto = req.body as UpdateProdutoDto;
+    try {
+        if (await checkNomeIsAvaliable(produto.nome, id)) {
+            const updatedProduto = await updateProduto(id, produto);
+            res.status(StatusCodes.NO_CONTENT).json();
+        } else {
+            res.status(StatusCodes.CONFLICT).json(ReasonPhrases.CONFLICT);
+        }
+    } catch(err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+};
+
+
+const remove = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const deletedProduto = await deleteProduto(id);
+        res.status(StatusCodes.NO_CONTENT).json();
+    } catch(err) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+};
 
 export default { index, create, read, update, remove };
 
--- WACAD013-DesenvolvimentoDeRESTfulAPIs/expApi/src/router/v1Router.ts
+++ WACAD013-DesenvolvimentoDeRESTfulAPIs/expApi/src/router/v1Router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import produtoRouter from "../resources/produto/produto.router";
+import languageRouter from "../resources/language/language.router";
 
 const router = Router();
 
 router.use("/produto", produtoRouter); // Quando for digitado o /produto será direcionado para o produtoRouter
+router.use("/language", languageRouter); // Quando for digitado o /produto será direcionado para o produtoRouter
+
 
 export default router;
