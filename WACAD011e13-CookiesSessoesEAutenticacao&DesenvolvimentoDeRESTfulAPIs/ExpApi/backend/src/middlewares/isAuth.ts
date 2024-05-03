82001bb4901e9c582d1cae0f79931e767d64478f
AdemarCastro <ademar.castro.fh@gmail.com>
2024-05-03 18:43:49 -0400

    [REFACTOR] - Corrige a codificação do isAuth.ts

--- WACAD011e13-CookiesSessoesEAutenticacao&DesenvolvimentoDeRESTfulAPIs/ExpApi/backend/src/middlewares/isAuth.ts
+++ WACAD011e13-CookiesSessoesEAutenticacao&DesenvolvimentoDeRESTfulAPIs/ExpApi/backend/src/middlewares/isAuth.ts
@@ -1,8 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { TiposUsuarios } from "../resources/tipoUsuario/tipoUsuario.constants";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
-export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-    if (req.session.tipoUsuarioId == TiposUsuarios.ADMIN) next();
-    else res.status(StatusCodes.FORBIDDEN).json(ReasonPhrases.FORBIDDEN);
-}
+export const isAuth = (req: Request, res: Response, next: NextFunction ) => {
+    if (req.session.uid) next();
+    res.status(StatusCodes.FORBIDDEN).json(ReasonPhrases.FORBIDDEN);
+};
